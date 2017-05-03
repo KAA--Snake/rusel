6a62f27cb4097f1f0a20752db2f0586afa645a4e
Serg <smu_139@mail.ru>
2017-05-03 22:56:54 +0300

    refr

--- common/config/main.php
+++ common/config/main.php
@@ -5,5 +5,13 @@ return [
         'cache' => [
             'class' => 'yii\caching\FileCache',
         ],
+        'urlManager' => [
+            'enablePrettyUrl' => true,
+            'showScriptName' => false,
+            'rules' => [
+                //правило для показа элементов типа post/show/1 (post/show?id=1)
+                'search/by-name/<name:\w+>' => 'search/by-name'
+            ],
+        ],
     ],
 ];
--- frontend/controllers/SearchController.php
+++ frontend/controllers/SearchController.php
@@ -4,6 +4,7 @@
  */
 namespace frontend\controllers;
 
+set_time_limit(0);
 
 use common\models\B_iblock_element;
 use common\models\elasticsearch\Element;
@@ -15,30 +16,59 @@ class SearchController extends Controller
     private $elementModel;
 
 
+    public function actionByName(){
+        $name = \Yii::$app->request->get('name');
+        echo 'Поиск по ' . $name;
+
+        $matches = Element::find()
+            ->query([
+                'match' => [
+                    'name' => $name,
+                ],
+            ])->orderBy(['quantity' => ['order' => 'desc']])
+            //'sort' => ['quantity' => ['order' => 'desc']]
+            ->limit(20)
+            ->all();
+
+        foreach($matches as $oneMatch){
+            $attr = $oneMatch->getAttributes();
+            \Yii::$app->pr->print_r2($attr);
+        }
+
+
+
+
+        return 'searched';
+    }
+
+
+
+
+
+
+
     public function actionImport(){
 
         //получим список товаров
 
         $this->elementModel = B_iblock_element::find()
             ->select( [
-                'b_iblock_element.ID',
-                'b_iblock_element.NAME',
-                'b_catalog_product.QUANTITY',
+                'b_iblock_element.ID as ID',
+                'b_iblock_element.NAME as NAME',
+                'b_catalog_product.QUANTITY as QUANTITY',
             ] )
             ->andWhere( ['b_iblock_element.IBLOCK_ID' => 66] )
             ->joinWith( 'bCatalogProduct' )
             ->isActive()
-            ->limit(5)
+            ->limit(50)
             ->orderBy( ['b_catalog_product.QUANTITY' => SORT_DESC] )
             ->all();
 
-        //$elementModel = new B_iblock_element();
-        //$elementModel->find()->Where(['b_iblock_element.IBLOCK_ID' => 66])->limit(5)->all();
-
         $this->__fillElements();
-        die();
 
-        return $this->render('search', []);
+        return 'imported';
+
+        //return $this->render('search', []);
     }
 
 
@@ -51,26 +81,58 @@ class SearchController extends Controller
             foreach($this->elementModel as $oneElement){
 
                 //echo $oneElement->getID() . '<br>';
+                echo $oneElement->ID . '<br>';
+                echo $oneElement->NAME . '<br>';
+                echo $oneElement->bCatalogProduct->QUANTITY . '<br>';
+                echo '<hr>';
+
+                if(!empty($oneElement->ID) && !empty($oneElement->NAME) && !empty($oneElement->bCatalogProduct->QUANTITY)){
+
+                    $elasticModel = new Element();
+                    $elasticModel->primaryKey = $oneElement->ID;
+
+                    $elasticModel->id = $oneElement->ID;
+                    $elasticModel->quantity = $oneElement->bCatalogProduct->QUANTITY;
+                    $elasticModel->name = $oneElement->NAME;
 
 
+                    if ($elasticModel->insert()) {
+                        //echo "Added Successfully <br>";
+                    } else {
+                        echo "Error <br>";
+                        echo $oneElement->ID . '<br>';
+                        echo $oneElement->NAME . '<br>';
+                        echo $oneElement->bCatalogProduct->QUANTITY . '<br>';
+                        echo '<hr>';
 
-                $rr = $oneElement->toArray();
-                \Yii::$app->pr->print_r2($rr);
+                    }
 
-                /*$elasticModel = new Element();
-                $elasticModel->name = 'Smu'.$i;
-                $elasticModel->surname = 'Solo'.$i;
-                $elasticModel->primaryKey = $i;
-                if ($elasticModel->insert()) {
-                    //echo "Added Successfully <br>";
-                } else {
-                    echo "Error <br>";
-                }*/
+                    //unset($elasticModel);
+                }
+
+                //$rr = $oneElement->toArray();
+                //\Yii::$app->pr->print_r2($rr);
 
             }
         }
+    }
 
 
 
+
+
+    /**
+     * Создаем индекс для модели customer
+     */
+    public function actionCreateIndex(){
+        Element::createIndex();
+        return 'index created';
+    }
+    /**
+     * Удаление индекса
+     */
+    public function actionDeleteIndex(){
+        Element::deleteIndex();
+        return 'index deleted';
     }
 }
