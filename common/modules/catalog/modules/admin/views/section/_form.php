17970173566482194f969982a643d77f4792601b
Serg <smu_139@mail.ru>
2017-05-24 21:47:19 +0300

    cat adm module sect crud

---
+++ common/modules/catalog/modules/admin/views/section/_form.php
@@ -0,0 +1,39 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model common\modules\catalog\models\Section */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="section-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'unique_id')->textInput() ?>
+
+    <?= $form->field($model, 'depth_level')->textInput() ?>
+
+    <?= $form->field($model, 'parent_id')->textInput() ?>
+
+    <?= $form->field($model, 'code')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'name')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'preview_text')->textarea(['rows' => 6]) ?>
+
+    <?= $form->field($model, 'detail_text')->textarea(['rows' => 6]) ?>
+
+    <?= $form->field($model, 'picture')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'sort')->textInput() ?>
+
+    <div class="form-group">
+        <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
---
+++ common/modules/catalog/modules/admin/views/section/index.php
@@ -0,0 +1,41 @@
+<?php
+
+use yii\helpers\Html;
+use yii\grid\GridView;
+use yii\widgets\Pjax;
+/* @var $this yii\web\View */
+/* @var $searchModel common\modules\catalog\models\SectionSearch */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = 'Sections';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="section-index">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+
+    <p>
+        <?= Html::a('Create Section', ['create'], ['class' => 'btn btn-success']) ?>
+    </p>
+<?php Pjax::begin(); ?>    <?= GridView::widget([
+        'dataProvider' => $dataProvider,
+        'filterModel' => $searchModel,
+        'columns' => [
+            ['class' => 'yii\grid\SerialColumn'],
+
+            'id',
+            'unique_id',
+            'depth_level',
+            'parent_id',
+            'code',
+            // 'name',
+            // 'preview_text:ntext',
+            // 'detail_text:ntext',
+            // 'picture',
+            // 'sort',
+
+            ['class' => 'yii\grid\ActionColumn'],
+        ],
+    ]); ?>
+<?php Pjax::end(); ?></div>
