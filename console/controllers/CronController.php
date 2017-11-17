2ec994898bb296b8b5e01a49e1b216d18d9ec6bf
Serg <sergey@klenmarket.ru>
2017-11-17 17:15:50 +0300

    раббит

--- console/controllers/CronController.php
+++ console/controllers/CronController.php
@@ -8,9 +8,14 @@
 
 namespace console\controllers;
 
+set_time_limit(0);
 
 use common\modules\catalog\models\currency\Currency;
+use PhpAmqpLib\Connection\AMQPStreamConnection;
+use PhpAmqpLib\Message\AMQPMessage;
 use yii\console\Controller;
+use yii\console\Exception;
+
 
 class CronController extends Controller
 {
@@ -23,6 +28,95 @@ class CronController extends Controller
         Currency::updateCurrencies();
         file_put_contents('/webapp/CronGetCurrency', date("d/m/Y"));
     }
+
+
+    /**
+     * Раббит демон тут
+     */
+    public function actionRabbitListen()
+    {
+
+        //$connection = new AMQPStreamConnection('rabbit', 5672, 'rabbit_user', 'rabbit3457');
+        try{
+            $connection = new AMQPStreamConnection('rabbit', 5672, 'user', 'pass');
+            $channel = $connection->channel();
+
+            $channel->queue_declare(
+                'invoice_queue',	#queue name - Имя очереди может содержать до 255 байт UTF-8 символов
+                false,      	#passive - может использоваться для проверки того, инициирован ли обмен, без того, чтобы изменять состояние сервера
+                true,      	#durable - убедимся, что RabbitMQ никогда не потеряет очередь при падении - очередь переживёт перезагрузку брокера
+                false,      	#exclusive - используется только одним соединением, и очередь будет удалена при закрытии соединения
+                false       	#autodelete - очередь удаляется, когда отписывается последний подписчик
+
+            );
+
+            /**
+             * не отправляем новое сообщение на обработчик, пока он
+             * не обработал и не подтвердил предыдущее. Вместо этого
+             * направляем сообщение на любой свободный обработчик
+             */
+            $channel->basic_qos(
+                null,   #размер предварительной выборки - размер окна предварительнйо выборки в октетах, null означает “без определённого ограничения”
+                1,  	#количество предварительных выборок - окна предварительных выборок в рамках целого сообщения
+                null	#глобальный - global=null означает, что настройки QoS должны применяться для получателей, global=true означает, что настройки QoS должны применяться к каналу
+            );
+
+            /**
+             * оповещает о своей заинтересованности в получении
+             * сообщений из определённой очереди. В таком случае мы
+             * говорим, что они регистрируют получателя, или устанавливают
+             * подписку на очередь. Каждый получатель (подписка) имеет
+             * идентификатор, называемый “тег получателя”.
+             */
+            $channel->basic_consume(
+                'invoice_queue',    	#очередь
+                '',                  #тег получателя - Идентификатор получателя, валидный в пределах текущего канала. Просто строка
+                false,               #не локальный - TRUE: сервер не будет отправлять сообщения соединениям, которые сам опубликовал
+                false,               #без подтверждения - false: подтверждения включены, true - подтверждения отключены. отправлять соответствующее подтверждение обработчику, как только задача будет выполнена
+                false,                 #эксклюзивная - к очереди можно получить доступ только в рамках текущего соединения
+                false,                 #не ждать - TRUE: сервер не будет отвечать методу. Клиент не должен ждать ответа
+                array($this, 'process')	#функция обратного вызова - метод, который будет принимать сообщение
+            );
+
+            while(count($channel->callbacks)) {
+                //$this->log->addInfo('Слежу за входящими сообщениями');
+                $channel->wait();
+            }
+
+            $channel->close();
+            $connection->close();
+        }
+        catch(Exception $e){
+            file_put_contents('/webapp/RabbitListen', 'error' .$e->getMessage(), FILE_APPEND);
+
+        }
+
+    }
+
+    /**
+     * обработка полученного запроса
+     *
+     * @param AMQPMessage $msg
+     */
+    public function process(AMQPMessage $msg)
+    {
+        //$this->generatePdf()->sendEmail();
+
+
+        file_put_contents('/webapp/RabbitProcess', $msg->body, FILE_APPEND);
+
+        /**
+         * Если получатель умирает, не отправив подтверждения, брокер
+         * AMQP пошлёт сообщение другому получателю. Если свободных
+         * на данный момент нет - брокер подождёт до тех пор, пока
+         * освободится хотя-бы один зарегистрированный получатель
+         * на эту очередь, прежде чем попытаться заново доставить
+         * сообщение
+         */
+        $msg->delivery_info['channel']->basic_ack($msg->delivery_info['delivery_tag']);
+    }
+
+
 
 
     public function actionShow(){
--- frontend/controllers/OrderController.php
+++ frontend/controllers/OrderController.php
@@ -10,6 +10,8 @@ namespace frontend\controllers;
 
 
 use common\modules\catalog\models\Order;
+use PhpAmqpLib\Connection\AMQPStreamConnection;
+use PhpAmqpLib\Message\AMQPMessage;
 use yii\web\Controller;
 use Yii;
 
@@ -35,8 +37,35 @@ class OrderController extends Controller
 
         if($form_model->load(Yii::$app->request->post())){
 
+            //$connection = new AMQPStreamConnection('rabbit', 5672, 'rabbit_user', 'rabbit3457');
+            $connection = new AMQPStreamConnection('rabbit', 5672, 'user', 'pass');
+            //$connection = new AMQPConnection('localhost', 5672, 'guest', 'guest');
+            $channel = $connection->channel();
+
+            $channel->queue_declare(
+                'invoice_queue',	#queue name - Имя очереди может содержать до 255 байт UTF-8 символов
+                false,      	#passive - может использоваться для проверки того, инициирован ли обмен, без того, чтобы изменять состояние сервера
+                true,      	#durable - убедимся, что RabbitMQ никогда не потеряет очередь при падении - очередь переживёт перезагрузку брокера
+                false,      	#exclusive - используется только одним соединением, и очередь будет удалена при закрытии соединения
+                false       	#autodelete - очередь удаляется, когда отписывается последний подписчик
+            );
+
+            $msg = new AMQPMessage(
+                5738234,
+                array('delivery_mode' => 2) #создаёт сообщение постоянным, чтобы оно не потерялось при падении или закрытии сервера
+            );
+
+            $channel->basic_publish(
+                $msg,           	#сообщение
+                '',             	#обмен
+                'invoice_queue' 	#ключ маршрутизации (очередь)
+            );
+
+            $channel->close();
+            $connection->close();
+
             //$this->redirect()
-            return $this->redirect(Yii::$app->request->referrer ?: Yii::$app->homeUrl);
+            //return $this->redirect(Yii::$app->request->referrer ?: Yii::$app->homeUrl);
 
             //Yii::$app->pr->print_r2(Yii::$app->request->post());
 
