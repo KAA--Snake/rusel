0e925a9c382f6b8eb4e8ac2b8b92913ac65a4306
kaa--snake <kaa.virt@gmail.com>
2018-02-13 22:44:01 +0300

    типа главная админки

--- backend/views/site/index.php
+++ backend/views/site/index.php
@@ -6,3 +6,30 @@ $this->title = 'admin main page';
 ?>
 
 
+<div class="inner_wrap mw1180">
+    <h1 class="header_h1">Админка</h1>
+    <div class="dashboard">
+        <div class="dashboard_section">
+            <h3 class="dashboard_section-header">Главная страница</h3>
+            <div class="dashboard_section-body">
+                <a class="dashboard_section-link" href="/">Слайдер</a>
+                <a class="dashboard_section-link" href="/">Специальные предложения</a>
+                <a class="dashboard_section-link" href="/">Полезная информация</a>
+                <a class="dashboard_section-link" href="/">Новости</a>
+            </div>
+        </div>
+
+        <div class="dashboard_section">
+            <h3 class="dashboard_section-header">Верхние статические страницы</h3>
+            <div class="dashboard_section-body">
+                <a class="dashboard_section-link" href="/">О проекте</a>
+                <a class="dashboard_section-link" href="/">Оплата и доставка</a>
+                <a class="dashboard_section-link" href="/">Договор оферты</a>
+                <a class="dashboard_section-link" href="/">Сотрудничество</a>
+                <a class="dashboard_section-link" href="/">Вакансии</a>
+                <a class="dashboard_section-link" href="/">Контакты</a>
+            </div>
+        </div>
+    </div>
+</div>
+
