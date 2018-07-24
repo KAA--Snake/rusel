0da46cc7ac8c099b40cccff26a38736d2f7e3b01
Sergey Solomonov <solomonov@virtualhealth.com>
2018-07-24 17:33:29 +0300

    добавлена яндекс метрика

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -18,6 +18,7 @@ AppAsset::register($this);
 <head>
     <link rel="shortcut icon" type="image/ico" href="/img/favicon.ico"/>
     <meta charset="<?= Yii::$app->charset ?>">
+    <meta name="yandex-verification" content="e582749fff05335f" />
     <meta name="viewport" content="width=1200px, initial-scale=0.0">
     <meta name="format-detection" content="telephone=no">
     <?= Html::csrfMetaTags() ?>
@@ -32,6 +33,30 @@ AppAsset::register($this);
 </script>
 <!-- Excitel {/call button} -->
 <body>
+<script type="text/javascript" >
+    (function (d, w, c) {
+        (w[c] = w[c] || []).push(function() {
+            try {
+                w.yaCounter49726129 = new Ya.Metrika2({
+                    id:49726129,
+                    clickmap:true,
+                    trackLinks:true,
+                    accurateTrackBounce:true
+                });
+            } catch(e) { }
+        });
+        var n = d.getElementsByTagName("script")[0],
+            s = d.createElement("script"),
+            f = function () { n.parentNode.insertBefore(s, n); };
+        s.type = "text/javascript";
+        s.async = true;
+        s.src = "https://mc.yandex.ru/metrika/tag.js";
+        if (w.opera == "[object Opera]") {
+            d.addEventListener("DOMContentLoaded", f, false);
+        } else { f(); }
+    })(document, window, "yandex_metrika_callbacks2");
+</script>
+<noscript><div><img src="https://mc.yandex.ru/watch/49726129" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
 <?php $this->beginBody() ?>
 
 <div class="popup_container recall hidden">
--- frontend/views/layouts/main_v2.php
+++ frontend/views/layouts/main_v2.php
@@ -18,12 +18,37 @@ AppAsset2::register($this);
 <head>
     <link rel="shortcut icon" type="image/ico" href="/img/favicon.ico"/>
     <meta charset="<?= Yii::$app->charset ?>">
+    <meta name="yandex-verification" content="e582749fff05335f" />
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <?= Html::csrfMetaTags() ?>
     <title><?= Html::encode($this->title) ?></title>
     <?php $this->head() ?>
 </head>
 <body>
+<script type="text/javascript" >
+    (function (d, w, c) {
+        (w[c] = w[c] || []).push(function() {
+            try {
+                w.yaCounter49726129 = new Ya.Metrika2({
+                    id:49726129,
+                    clickmap:true,
+                    trackLinks:true,
+                    accurateTrackBounce:true
+                });
+            } catch(e) { }
+        });
+        var n = d.getElementsByTagName("script")[0],
+            s = d.createElement("script"),
+            f = function () { n.parentNode.insertBefore(s, n); };
+        s.type = "text/javascript";
+        s.async = true;
+        s.src = "https://mc.yandex.ru/metrika/tag.js";
+        if (w.opera == "[object Opera]") {
+            d.addEventListener("DOMContentLoaded", f, false);
+        } else { f(); }
+    })(document, window, "yandex_metrika_callbacks2");
+</script>
+<noscript><div><img src="https://mc.yandex.ru/watch/49726129" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
 <?php $this->beginBody() ?>
 
 <div class="wrap">
