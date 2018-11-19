602ea48f5518bb230171cb0a73ac3cb5d56a9579
kaa--snake <kaa.virt@gmail.com>
2018-11-20 00:55:45 +0300

    new filter on prod

--- common/widgets/filter/views/catalog_filter.php
+++ common/widgets/filter/views/catalog_filter.php
@@ -22,37 +22,37 @@
 
 
     <div class="filter_selector_wrap collapsed">
-        <table class="filter_tab">
+       <!-- <table class="filter_tab">
 
-            <?if(isset($filterManufacturers) && is_array($filterManufacturers)){?>
+            <?/*if(isset($filterManufacturers) && is_array($filterManufacturers)){*/?>
                 <tr class="filter_selector_item">
                     <td class="tags">
                         <ul class="tag_list">
                             <li data-param="manufacturer" class="name js-filter-param-name">Производитель:</li>
-                            <?foreach($filterManufacturers as $oneFilter){?>
-                                <li data-tag="<?=$oneFilter['key'];?>" class="tag_item js-filter-param-item"><?=$oneFilter['key'];?> <span class="filter_param_count">(<?=$oneFilter['doc_count'];?>)</span></li>
-                            <?}?>
+                            <?/*foreach($filterManufacturers as $oneFilter){*/?>
+                                <li data-tag="<?/*=$oneFilter['key'];*/?>" class="tag_item js-filter-param-item"><?/*=$oneFilter['key'];*/?> <span class="filter_param_count">(<?/*=$oneFilter['doc_count'];*/?>)</span></li>
+                            <?/*}*/?>
                         </ul>
                     </td>
                 </tr>
-            <?}?>
+            <?/*}*/?>
 
-            <?foreach($filterData as $key => $oneFilter){?>
+            <?/*foreach($filterData as $key => $oneFilter){*/?>
                 <tr class="filter_selector_item">
                     <td class="tags">
                         <ul class="tag_list">
-                            <li data-param="<?=$key;?>" class="name js-filter-param-name"><?=$oneFilter['prop_name'];?>:</li>
-                            <?foreach($oneFilter['prop_values']['buckets'] as $i => $oneBucket){?>
-                                <li data-tag="<?=$oneBucket['key'];?>" class="tag_item js-filter-param-item <?php if($i >= 10){?> hidden <?}?>"><?=$oneBucket['key'];?> <span class="filter_param_count">(<?=$oneBucket['doc_count'];?>)</span></li>
-                            <?}?>
-                            <?php if(count($oneFilter['prop_values']['buckets']) > 10) {?>
+                            <li data-param="<?/*=$key;*/?>" class="name js-filter-param-name"><?/*=$oneFilter['prop_name'];*/?>:</li>
+                            <?/*foreach($oneFilter['prop_values']['buckets'] as $i => $oneBucket){*/?>
+                                <li data-tag="<?/*=$oneBucket['key'];*/?>" class="tag_item js-filter-param-item <?php /*if($i >= 10){*/?> hidden <?/*}*/?>"><?/*=$oneBucket['key'];*/?> <span class="filter_param_count">(<?/*=$oneBucket['doc_count'];*/?>)</span></li>
+                            <?/*}*/?>
+                            <?php /*if(count($oneFilter['prop_values']['buckets']) > 10) {*/?>
                                 <li class="filter_more_btn js-filter-param-item-more">показать все ↓</li>
-                            <?}?>
+                            <?/*}*/?>
                         </ul>
                     </td>
                 </tr>
-            <?}?>
-        </table>
+            <?/*}*/?>
+        </table>-->
         <div class="apply_filter_btn_wrap">
 
             <form action="" method="post" class="filter_form hidden" name="productFilterForm" id="filter-form">
@@ -91,13 +91,14 @@
         <div class="filter_btn js-filter_dropdown inactive">Фильтр</div>
 
         <div class="filter_params_applied">
-            <div class="filter__inner_empty">Выбрать параметры для фильтра </div>
+            <div class="filter__inner_empty js-empty-filter">Выбрать параметры для фильтра </div>
             <div class="filter__inner_params">
                 <div class="filter_selector_wrap collapsed">
                     <?if(isset($filterManufacturers) && is_array($filterManufacturers)){?>
                         <div class="filter-group">
                             <div class="filter-group__name js-filter-param-name " data-param="manufacturer">Производитель:</div>
-                            <div class="filter-group__collapse-btn ">Выбрать из списка <span class="arrow"></span></div>
+                            <div class="filter-group__applied_params js-applied_filter_group"><span class="applied_filter_params_list js-applied_filter_params_list"></span><span class="cross js-cancel-filter-group"></span></div>
+                            <div class="filter-group__collapse-btn js-show-filter_group">Выбрать из списка <span class="arrow"></span></div>
                             <div class="filter-group__params-box collapsed">
                                 <ul class="filter-group__params-list js-scroll-pane">
                                     <?foreach($filterManufacturers as $oneFilter){?>
@@ -112,7 +113,8 @@
                     <?foreach($filterData as $key => $oneFilter){?>
                         <div class="filter-group">
                             <div class="filter-group__name js-filter-param-name " data-param="<?=$key;?>"><?=$oneFilter['prop_name'];?>:</div>
-                            <div class="filter-group__collapse-btn ">Выбрать из списка <span class="arrow"></span></div>
+                            <div class="filter-group__applied_params js-applied_filter_group"><span class="applied_filter_params_list js-applied_filter_params_list"></span><span class="cross js-cancel-filter-group"></span></div>
+                            <div class="filter-group__collapse-btn js-show-filter_group">Выбрать из списка <span class="arrow"></span></div>
                             <div class="filter-group__params-box collapsed">
                                 <ul class="filter-group__params-list js-scroll-pane">
                                     <?foreach($oneFilter['prop_values']['buckets'] as $i => $oneBucket){?>
